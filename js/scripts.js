23b383d964a67eb15a50bfed619b1718bd3a91cd
ris <iris.wong@zgm.ca>
2016-06-12 23:57:49 -0600

    working on outline for presentation.

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+var listItems = "";
+var index = 0;
+$("main h1").each(function() {
+		var id = $(this).attr("id");
+		var text = $(this).text();
+		var newtext = text.replace(/[^a-zA-Z ]/g, '').replace(/\s\s+/g, ' ').replace(/ /g, '').toLowerCase();
+
+		if (id == undefined)
+				$(this).attr("id", newtext);
+
+		id = $(this).attr("id");
+
+		console.log(window.location);
+
+		listItems += "<li><a href='" + window.location.pathname + "#" + id + "'>" + $(this).text() + "</a></li>";
+
+		// index++;
+});
+
+$("nav").append("<ul>" + listItems + "</ul>");
+
+$(function() {
+		$('a[href*=\\#]:not([href=\\#])').click(function() {
+				if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+						var target = $(this.hash);
+						target = target.length
+								? target
+								: $('[name=' + this.hash.slice(1) + ']');
+						if (target.length) {
+								$('html,body').animate({
+										scrollTop: target.offset().top
+								}, 500, 'easeInOutSine');
+								return false;
+						}
+				}
+		});
+});
+
+$(document).ready(function() {
+		$('code').each(function() {
+				$(this).text($(this).html());
+		});
+});
